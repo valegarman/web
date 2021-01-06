d5ed4b4a3e81442e6bda88ed7ca12369f70dcf8e
valegarman <valegarman@gmail.com>
2021-01-06 22:34:37 +0100

    Add funko animation on click

--- components/funko/funko.vue
+++ components/funko/funko.vue
@@ -4,7 +4,7 @@
       ref="canvas"
       :width="`${width * scale}px`"
       :height="`${height * scale}px`"
-      @click="onFunko"
+      @click="step"
     ></canvas>
   </div>
 </template>
@@ -12,66 +12,57 @@
 <script>
 export default {
   data: () => ({
+    img: null,
     scale: 0.3,
     width: 406,
     height: 599,
+    canvas: null,
+    ctx: null,
+    scaledWidth: null,
+    scaledHeight: null,
+    cycleLoop: [0, 1, 2, 0, 3, 5, 0, 4, 0, 4],
+    currentLoopIndex: 0,
+    frameCount: 0,
   }),
   mounted() {
-    const img = new Image()
-    img.src = '/images/funkoManu-406-sprit.png'
-    img.onload = function () {
-      init()
-    }
-    const canvas = this.$refs.canvas
-    const ctx = canvas.getContext('2d')
-
-    const scale = this.scale
-    const width = this.width
-    const height = this.height
-    const scaledWidth = scale * width
-    const scaledHeight = scale * height
-
-    function drawFrame(frameX, frameY, canvasX, canvasY) {
-      ctx.drawImage(
-        img,
-        frameX * width,
-        frameY * height,
-        width,
-        height,
-        canvasX,
-        canvasY,
-        scaledWidth,
-        scaledHeight
-      )
-    }
-
-    const cycleLoop = [0, 1, 2, 3, 4, 5]
-    let currentLoopIndex = 0
-    let frameCount = 0
-
-    function step() {
-      frameCount++
-      if (frameCount < 15) {
-        window.requestAnimationFrame(step)
-        return
-      }
-      frameCount = 0
-      ctx.clearRect(0, 0, canvas.width, canvas.height)
-      drawFrame(cycleLoop[currentLoopIndex], 0, 0, 0)
-      currentLoopIndex++
-      if (currentLoopIndex >= cycleLoop.length) {
-        currentLoopIndex = 0
-      }
-      window.requestAnimationFrame(step)
-    }
-
+    this.img = new Image()
+    this.img.src = '/images/funkoManu-406-sprit.png'
+    const step = this.step
     function init() {
       window.requestAnimationFrame(step)
     }
+    this.img.onload = function () {
+      init()
+    }
+    this.canvas = this.$refs.canvas
+    this.ctx = this.canvas.getContext('2d')
+
+    this.scaledWidth = this.scale * this.width
+    this.scaledHeight = this.scale * this.height
   },
   methods: {
-    onFunko() {
-      console.log('asdsahdgsadas')
+    drawFrame(frameX, frameY, canvasX, canvasY) {
+      this.ctx.drawImage(
+        this.img,
+        frameX * this.width,
+        frameY * this.height,
+        this.width,
+        this.height,
+        canvasX,
+        canvasY,
+        this.scaledWidth,
+        this.scaledHeight
+      )
+    },
+    step() {
+      this.frameCount++
+      this.frameCount = 0
+      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+      this.drawFrame(this.cycleLoop[this.currentLoopIndex], 0, 0, 0)
+      this.currentLoopIndex++
+      if (this.currentLoopIndex >= this.cycleLoop.length) {
+        this.currentLoopIndex = 0
+      }
     },
   },
 }
