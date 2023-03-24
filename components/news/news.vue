be8b45ce95befbc6596c69655a2135553f5a19be
Sergio Valero García <sergiovalero92@gmail.com>
2023-03-24 11:39:03 +0100

    feat: add mosaic for news

---
+++ components/news/news.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="news">
+    <div v-for="pieceOfNews in news" :key="pieceOfNews.name">
+      <pieceOfNewsCard :piece-of-news="pieceOfNews" />
+    </div>
+  </div>
+</template>
+
+<script>
+import pieceOfNewsCard from '~/components/news/pieceOfNewsCard.vue'
+
+export default {
+  components: {
+    pieceOfNewsCard,
+  },
+  props: {
+    news: {
+      type: Array,
+      default: () => [],
+    },
+  },
+}
+</script>
+
+<style scoped>
+.news {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+</style>
